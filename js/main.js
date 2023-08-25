30bb2e2f24d02abeeaee36b24d3018382cd91d72
dwnorm2 <dwnorm2@gmail.com>
2023-08-25 18:54:01 -0500

    put back in ios orientation js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-//fix viewport change on orientation change
-// if (
-//   navigator.userAgent.match(/iPhone/i) ||
-//   navigator.userAgent.match(/iPad/i)
-// ) {
-//   var viewportmeta = document.querySelector('meta[name="viewport"]');
-//   if (viewportmeta) {
-//     viewportmeta.content =
-//       "width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0";
-//     document.addEventListener(
-//       "gesturestart",
-//       function () {
-//         viewportmeta.content =
-//           "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-//       },
-//       false
-//     );
-//   }
-// }
+// fix viewport change on orientation change
+if (
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i)
+) {
+  var viewportmeta = document.querySelector('meta[name="viewport"]');
+  if (viewportmeta) {
+    viewportmeta.content =
+      "width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0";
+    document.addEventListener(
+      "gesturestart",
+      function () {
+        viewportmeta.content =
+          "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+      },
+      false
+    );
+  }
+}
 //smooth scroll
 // document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 //   anchor.addEventListener("click", function (e) {
