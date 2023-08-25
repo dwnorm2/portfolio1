50698e7c0395f567be509b720d5dad5fed4faccc
dwnorm2 <dwnorm2@gmail.com>
2023-08-25 13:17:44 -0500

    add static wave animation

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@
 //   }
 // }
 //smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
