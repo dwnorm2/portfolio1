961bd862f2eeb2e199f434a4bd6353a450965062
dwnorm2 <dwnorm2@gmail.com>
2023-08-29 18:26:32 -0500

    fix animation reset

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ if (
     );
   }
 }
-//smooth scroll
+// smooth scroll
 // document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 //   anchor.addEventListener("click", function (e) {
 //     e.preventDefault();
